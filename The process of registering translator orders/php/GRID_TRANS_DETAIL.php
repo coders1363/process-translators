43344a8d8f05f3640333065523b909ac2b66d398
coders1363 <coders.pgm@gmail.com>
2024-08-18 15:29:50 +0330

    edite

--- The process of registering translator orders/php/GRID_TRANS_DETAIL.php
+++ The process of registering translator orders/php/GRID_TRANS_DETAIL.php
@@ -1,50 +1,36 @@
 <?php
 
 try {
-    // دریافت مقدار شناسه درخواست
-    $app_uid = @@APPLICATION;
+	// دریافت مقدار شناسه درخواست
+	$app_uid = @@APPLICATION;
 
-    // کوئری برای دریافت اطلاعات مرتبط با شناسه درخواست
-    $fetchRequestDetailsQuery = "SELECT 
-    pt.APP_UID,
-        pt.APP_NUMBER,
-        pt.APP_STATUS,
-        pt.TXT_NAME,
-        pt.TXT_EMAIL,
-        pt.TXT_MOBILE,
-        pt.DRP_TRANSLATION_FIELD,
-        pt.HIDE_SOURCE_APP_UID,
-        pt.DRP_TRANSLATION_FIELD_LABEL,
-        pt.TXT_NAME_TRANSLATE,
-        pudt.TXT_TRANSLATION_TIME,
-        pudt.TXT_TRANSLATION_COST
-FROM `pmt_translate` pt
-INNER JOIN pmt_update_db_trans pudt ON pt.APP_UID = pudt.HIDE_SOURCE_APP_UID
-WHERE pt.APP_UID = '$app_uid' AND pudt.RAD_ACCEPT_TRANS = '1'";
+	
+	// کوئری برای دریافت اطلاعات مرتبط با شناسه درخواست
+	$fetchRequestDetailsQuery = "SELECT `APP_UID`, `APP_NUMBER`, `APP_STATUS`, `TXT_NAME_TRANSLATE`,`HIDE_SOURCE_APP_UID`, `TXT_TRANSLATION_TIME`, `TXT_TRANSLATION_COST` FROM `pmt_translate`
+WHERE HIDE_SOURCE_APP_UID= '$app_uid' AND RAD_ACCEPT_TRANS = '1'";
 
-    // اجرای کوئری
-    $resultMain = executeQuery($fetchRequestDetailsQuery);
+	// اجرای کوئری
+	$resultMain = executeQuery($fetchRequestDetailsQuery);
+
 	//echo '<pre>';var_dump($fetchRequestDetailsQuery);die();
-//dd($resultMain);
-    // بررسی وجود نتایج و تنظیم مقادیر گرید
-    if (count($resultMain) > 0) {
-        $review_list = [];
-        foreach ($resultMain as $key => $app) {
-            $review_list[$key] = [
-                'txt_name' => $app['TXT_NAME'],
-                'txt_email' => $app['TXT_EMAIL'],
-                'txt_mobile' => $app['TXT_MOBILE'],
-                'drp_Translation_field' => $app['DRP_TRANSLATION_FIELD'],
-                'txt_Translation_time' => $app['TXT_TRANSLATION_TIME'],
-                'txt_Translation_cost' => $app['TXT_TRANSLATION_COST']
-            ];
-        }
+	//dd($fetchRequestDetailsQuery);
+	// بررسی وجود نتایج و تنظیم مقادیر گرید
+	if (count($resultMain) > 0) {
+		$review_list = [];
+		foreach ($resultMain as $key => $app) {
+			$review_list[$key] = [
+				'txt_id_translate' => $app['APP_UID'],
+				'txt_name_translate' => $app['TXT_NAME_TRANSLATE'],
+				'txt_Translation_time' => $app['TXT_TRANSLATION_TIME'],
+				'txt_Translation_cost' => $app['TXT_TRANSLATION_COST']
+			];
+		}
 
-        // تنظیم مقادیر گرید
-        @@grd_trans_details = $review_list; // اصلاح شده
-    }
+		// تنظیم مقادیر گرید
+		@@grd_trans_details = $review_list; // اصلاح شده
+	}
 
 } catch (Exception $e) {
-    // مدیریت استثناها و نمایش خطا
-    die("خطا: " . $e->getMessage());
+	// مدیریت استثناها و نمایش خطا
+	die("خطا: " . $e->getMessage());
 }
